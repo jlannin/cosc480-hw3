195c733d92a441595072bae7d89d7a1ae27b396b
jlannin <jlannin@colgate.edu>
2015-02-18 02:42:08 -0500

    Commiting! First two methods of part 1 (model) working!

---
+++ app/models/product.rb
@@ -0,0 +1,29 @@
+class Product < ActiveRecord::Base
+  def age_range
+    if self.minimum_age_appropriate == nil
+      "0 and above"
+    elsif self.maximum_age_appropriate == nil
+      "#{self.minimum_age_appropriate} and above"
+    elsif self.minimum_age_appropriate == self.maximum_age_appropriate
+      "Age #{self.minimum_age_appropriate}"
+    else
+      "#{self.minimum_age_appropriate} to #{self.maximum_age_appropriate}"
+    end
+  end
+
+  def age_appropriate?(age)
+    if age < 0
+      false
+    elsif self.minimum_age_appropriate == nil
+      true
+    elsif self.minimum_age_appropriate > age #past this point we know above min age
+      false
+    elsif self.maximum_age_appropriate == nil
+      true
+    elsif self.maximum_age_appropriate < age
+      false
+    else
+      true
+    end
+  end
+end
